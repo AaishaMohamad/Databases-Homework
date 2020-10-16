6e256cb59d563c753ca5c84109af2e78e42af298
Mawaddah <itzolda@gmail.com>
2020-10-16 13:19:16 +0100

    Created mentors table

---
+++ week-1/mandatory/2-classes-db/week1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE mentors(
+    id                          SERIAL PRIMARY KEY,
+    name                        VARCHAR(30),
+    years_in_glasgow            VARCHAR(30),
+    address                     VARCHAR(120),
+    fav_Programming_language    VARCHAR(30)
+
+);
